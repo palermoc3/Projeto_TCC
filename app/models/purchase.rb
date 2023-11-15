e00d35275113a7239c1536da5b6896feaa18ac79
palermoc <pedropalermo97@gmail.com>
2023-11-15 12:45:49 -0300

    Desc-16/Method to the correios finalize of purchase

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,7 +1,7 @@
 class Purchase < ApplicationRecord
   belongs_to :user
   has_many :item_purchases
-
+  #require 'correios_gem'
   def update_price
     self.price = item_purchases.sum { |item| item.quantity * item.product.valueBuy }
     save
@@ -11,4 +11,24 @@ class Purchase < ApplicationRecord
     self.packSize = item_purchases.sum { |item| item.product.width * item.product.length * item.product.depth }
     save
   end
+
+    #def update_pack_price
+    #result = Correios::Pricefier.calculate_price({
+    #service_codes: ['Não tenho', 'Não tenho'], Preciso de credencias oficiais para usar
+    #source_zip_code: User.find_by_id(1).cep,
+    #target_zip_code: user&.cep,
+    #object: {
+    #  type: :box,
+    #  weight: self.packSize = item_purchases.sum { |item| item.product.width * item.product.length * item.product.depth },
+    
+    #  length: self.packSize = item_purchases.sum { |item|  item.product.length },
+    #  height: self.packSize = item_purchases.sum { |item|  item.product.depth },
+    #  width: self.packSize =  item_purchases.sum { |item|  item.product.width },
+    #  diameter: nil
+    #},
+    #own_hands: true,
+    #receipt_notification: false,
+    #declared_value: self.price
+  #})
+  #end
 end
