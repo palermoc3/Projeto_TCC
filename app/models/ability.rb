96ae58d6b3d379011f57857f52e57fb924c0391b
palermoc <pedropalermo97@gmail.com>
2023-11-15 14:05:47 -0300

    Desc-19/ Create ability of Store and Administrator

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,15 +10,25 @@ class Ability
       can :manage, About
       can [:manage], Cellphone , user_id: user.id
       can [:read], Cellphone 
+      can [:update, :read], Store
+      can  [:update, :read], Administrator
       # Habilidades para usuários padrão (se necessário)
     elsif user.present? && user.role == 2
       can [:read, :create, :update,], About
       can [:manage], Cellphone , user_id: user.id
+      can  :read, Store
+      can  :read, Administrator
     elsif user.present? && user.role == 3 
       can [:manage], Cellphone , user_id: user.id
       can :read, About
+      can  :read, Store
+      can  :read, Administrator
+
     else
       can :read, About
+      can  :read, Store
+      can  :read, Administrator
+
     end
     
   end
