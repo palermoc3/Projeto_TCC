5bf280b919655768807b01b19cd1588895d9f584
palermoc <pedropalermo97@gmail.com>
2023-11-15 14:19:14 -0300

    Desc-20/ Create ability of BD (miss Photo)

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -12,23 +12,45 @@ class Ability
       can [:read], Cellphone 
       can [:update, :read], Store
       can  [:update, :read], Administrator
+      can [:read, :update], User
+      can [:read, :update], Purchase
+      can [:read, :update], ItemPurchases , user_id: user.id
+      can [:read], ItemPurchases
+      can [:read, :update], Payroll
+      can [:read, :update, :create], Product
+      can [:read, :update, :create], SubCategory
+      can [:read, :update, :create], Category
       # Habilidades para usuários padrão (se necessário)
     elsif user.present? && user.role == 2
       can [:read, :create, :update,], About
       can [:manage], Cellphone , user_id: user.id
       can  :read, Store
       can  :read, Administrator
+      can [:read, :update], User , user_id: user.id
+      can [:read, :update,:create], Purchase , user_id: user.id
+      can [:read, :update], ItemPurchases , user_id: user.id
+      can :read, Payroll , user_id: user.id
+      can :read, Product
+      can :read, SubCategory
+      can :read, Category
     elsif user.present? && user.role == 3 
       can [:manage], Cellphone , user_id: user.id
       can :read, About
       can  :read, Store
       can  :read, Administrator
-
+      can [:manage], User , user_id: user.id
+      can [:read, :update, :create], Purchase , user_id: user.id
+      can [:read, :update], ItemPurchases , user_id: user.id
+      can :read, Product
+      can :read, SubCategory
+      can :read, Category
     else
       can :read, About
       can  :read, Store
       can  :read, Administrator
-
+      can :read, Product
+      can :read, SubCategory
+      can :read, Category
     end
     
   end
